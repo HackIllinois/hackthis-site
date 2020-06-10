9252f9fd3768709195bc962fac69d1fc797cce88
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-06-10 15:23:37 -0500

    Set up React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HackIllinois 2021 presents HackThis Coming August!
+    </div>
+  );
+}
+
+export default App;
