0cc99c3d6c08a13357fc0dfc37318b9a0622fe89
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-06-13 14:01:32 -0500

    Add all text content

--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 import styles from './style.module.scss';
+import logo from 'assets/logo.svg';
 
 const Home = () => (
-  <div className={styles.home}>HackIllinois 2021 presents HackThis Coming August!</div>
+  <main className={styles.home}>
+    <nav className={styles.navbar}>
+      <img className={styles.logo} src={logo} alt="HackIllinois Logo" />
+    </nav>
+
+    <section className={styles.top}>
+      <div className={styles.content}>
+        <h3 className={styles.presents}>HackIllinois Presents</h3>
+        <h1 className={styles.title}>HackThis</h1>
+        <h2 className={styles.tagline}>this.hack = education</h2>
+        <h4 className={styles.dates}>August 7 - August 15, 2020</h4>
+      </div>
+    </section>
+  </main>
 );
 
 export default Home;
