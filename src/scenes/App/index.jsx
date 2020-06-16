93e42ddf6a050546e5bf8e04a73f0181f2068edb
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-06-16 10:56:29 -0500

    Add support for (almost) all screen sizes

--- src/scenes/App/index.jsx
+++ src/scenes/App/index.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import styles from './style.module.scss';
-import logo from 'assets/logo.svg';
 import Home from 'scenes/Home';
 
 const App = () => (
   <Router>
-    <header className={styles.navbar}>
-      <img className={styles.logo} src={logo} alt="HackIllinois Logo" />
-      <nav></nav>
-    </header>
-
     <Switch>
       <Route path="/">
         <Home />
--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 import styles from './style.module.scss';
 import background from 'assets/home/background.svg';
@@ -10,9 +10,9 @@ import globe from 'assets/home/globe.svg';
 import notebook from 'assets/home/notebook.svg';
 import paintTools from 'assets/home/paint_tools.svg';
 import titration from 'assets/home/titration.svg';
+import NavBar from 'components/NavBar';
 
 const decorations = [
-  // { asset: background, style: { top: 0, right: 0 } },
   { asset: pencil, style: { top: 452, right: 492 } },
   { asset: paper, style: { top: 140, right: 883 } },
   { asset: calculator, style: { top: 89, right: 674 } },
@@ -37,22 +37,34 @@ const dots = [
 ];
 
 const mobileDecorations = [
-  // { asset: mobileBackground, style: { top: 0, right: 0 } },
-  { asset: pencil, style: { top: 275, right: 278, /*transform: 'rotate(-4.47deg)'*/ } },
-  // { asset: paper, style: {}}
+  { asset: pencil, style: { top: 282, left: 75, width: 93, transform: 'rotate(-4.47deg)' } },
+  { asset: paper, style: { top: 117, left: 57, width: 55, transform: 'rotate(-17.37deg)' } },
+  { asset: titration, style: { top: 108, left: 163, width: 51, transform: 'rotate(-2.86deg)' } },
+  { asset: notebook, style: { top: 81, left: 252, width: 55, } },
+  { asset: calculator, style: { top: 103, left: 364, width: 26, transform: 'rotate(11.21deg)' } },
+  { asset: globe, style: { top: 196, left: 282, width: 64, transform: 'rotate(15.45deg)' } },
 ];
+
+const mobileDots = [
+  { top: 90, left: 42, width: 14, height: 6 },
+  { top: 117, left: 134, width: 4, height: 4 },
+  { top: 46, left: 164, width: 10, height: 10 },
+  { top: 232, left: 165, width: 20, height: 8 },
+  { top: 199, left: 248, width: 9, height: 9 },
+  { top: 147, left: 315, width: 4, height: 4 },
+  { top: 51, left: 349, width: 6, height: 6 },
+  { top: 313, left: 384, width: 9, height: 9 },
+  { top: 202, left: 405, width: 25, height: 12 },
+]
+
 const Home = () => {
   const [backgroundWidth, setBackgroundWidth] = useState(0);
   const [backgroundHeight, setBackgroundHeight] = useState(0);
 
-  const backgroundRef = useCallback(img => {
-    if (img !== null) {
-      img.addEventListener('load', () => {
-        setBackgroundWidth(img.offsetWidth);
-        setBackgroundHeight(img.offsetHeight);
-      });
-    }
-  });
+  const setBackgroundDimensions = img => {
+    setBackgroundWidth(img.offsetWidth);
+    setBackgroundHeight(img.offsetHeight);
+  }
   
   const backgroundStyleVariables = {
     '--background-width': `${backgroundWidth}px`,
@@ -60,7 +72,9 @@ const Home = () => {
   }
 
   return (
-    <main className={styles.home} style={backgroundStyleVariables}>
+    <div className={styles.home} style={backgroundStyleVariables}>
+      <NavBar />
+
       <section className={styles.top}>
         <div className={styles.content}>
           <h3 className={styles.presents}>HackIllinois Presents</h3>
@@ -73,7 +87,7 @@ const Home = () => {
           <div className={styles.spacer} />
 
           <div className={styles.decorations}>
-            <img src={background} alt="" ref={backgroundRef}/>
+            <img src={background} alt="" onLoad={e => setBackgroundDimensions(e.target)}/>
 
             {decorations.map(({ asset, style }) => (
               <img className={styles.decoration} src={asset} alt="" style={style} />
@@ -91,9 +105,13 @@ const Home = () => {
           {mobileDecorations.map(({ asset, style }) => (
             <img className={styles.decoration} src={asset} alt="" style={style} />
           ))}
+
+          {mobileDots.map(style => (
+            <div className={styles.dot} style={style} />
+          ))}
         </div>
       </section>
-    </main>
+    </div>
   );
 }
 
