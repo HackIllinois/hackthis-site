5d8ad5c5241003c2ce5ea9c234fef98e14b37f3c
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-07-09 16:26:49 -0700

    Add event info and faq to home page (#14)
    * Add event info and faq to home page
    
    * Replace 'apply' with 'register'
    
    * Change endpoint to /register
    
    * Minor style fixes
    
    * Bold github note and add 'years' to programming years dropdown options
    
    * Add logo to registration pages
    
    * Update event info

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import styles from './style.module.scss';
 import logo from 'assets/logo.svg';
-import logoWhite from 'assets/logo_white.svg';
+import logoLight from 'assets/logo_light.svg';
 
 const NavBar = () => (
   <header className={styles.navbar}>
     <img className={styles.logo} src={logo} alt="HackIllinois Logo" />
-    <img className={styles.logoMobile} src={logoWhite} alt="HackIllinois Logo" />
+    <img className={styles.logoMobile} src={logoLight} alt="HackIllinois Logo" />
     <nav></nav>
   </header>
 );
